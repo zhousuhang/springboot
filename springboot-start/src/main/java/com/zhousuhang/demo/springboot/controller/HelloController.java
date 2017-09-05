4b1eb32ded111bf7c1acb791c5e96ef50f579981
Suhang <zhousuhang@163.com>
2017-09-05 11:59:11 +0800

    test

--- springboot-start/src/main/java/com/zhousuhang/demo/springboot/controller/HelloController.java
+++ springboot-start/src/main/java/com/zhousuhang/demo/springboot/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.zhousuhang.demo.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@RequestMapping("/hello")
 	public String index() {
+		logger.debug("Hello Spring Boot.");
 		return "Hello Spring Boot.";
 	}
 }
